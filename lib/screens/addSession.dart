8e9e75bcb0ed634015f078810fc871a7ce5fbb30
bodrulamin <bodrulaminiu@gmail.com>
2021-02-26 02:44:53 +0600

    cardview commit

--- lib/screens/addSession.dart
+++ lib/screens/addSession.dart
@@ -1,4 +1,7 @@
+
+
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:iscacentral/model/persondetails.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -24,25 +27,22 @@ class _SessionBodyState extends State<SessionBody> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Container(
+      child: SizedBox(
         width: 500,
-        alignment: Alignment.center,
-        child: ListView(
+        child: Column(
+
           children: [
-            Column(
-              children: [
-                Text("data"),
-                SizedBox(
-                    height: 500,
-                    child: ListView.builder(
-                        itemBuilder: personCard, itemCount: persons.length)),
-                ElevatedButton(
-                    onPressed: () {
-                      showDialog(context: context, builder: dialogView);
-                    },
-                    child: Text("Add Person"))
-              ],
+
+            Expanded(
+
+              child: ListView.builder(
+                  itemBuilder: personCard, itemCount: persons.length),
             ),
+            ElevatedButton(
+                onPressed: () {
+                  showDialog(context: context, builder: dialogViewForInput);
+                },
+                child: Text("Add Person")),
           ],
         ),
       ),
@@ -51,62 +51,82 @@ class _SessionBodyState extends State<SessionBody> {
 
   Widget personCard(BuildContext context, int index) {
     return Card(
-      child: Column(
+      child: ExpansionTile(
+
+        leading: FaIcon(FontAwesomeIcons.solidUser),
+        collapsedBackgroundColor: Colors.lime.shade50,
+        expandedAlignment: Alignment.bottomLeft,
+        expandedCrossAxisAlignment: CrossAxisAlignment.start,
+        childrenPadding: EdgeInsets.only(left: 20.0),
+        subtitle:   Text(persons[index].designation),
+        tilePadding: EdgeInsets.only(left: 25.0),
+        title: Text(
+          persons[index].name,
+          style: TextStyle(color: Colors.blueAccent, fontSize: 25.0),
+        ),
         children: [
-          Text(persons[index].name),
-          Text(persons[index].designation),
-          TextButton(
-              onPressed: () {
-                launch("tel: ${persons[index].phone}");
-              },
-              child: Text(persons[index].phone)),
-          Text(persons[index].email),
-          Text(persons[index].fbLink),
+          ListTile(
+              isThreeLine: false,
+
+              leading: Icon(Icons.phone),
+              onTap: ()=>
+                  launch("tel:${persons[index].phone}"),
+              title: Text(persons[index].phone)),
+          ListTile(
+
+              leading: Icon(Icons.email),
+              onTap: () => launch("mailto:${persons[index].email}"),
+              title: Text(persons[index].email, )
+          ),
+          ListTile(
+            leading: FaIcon(FontAwesomeIcons.facebook),
+            onTap: () => launch(persons[index].fbLink),
+            title: Text(persons[index].fbLink),
+          ),
         ],
       ),
     );
   }
 
-  Widget dialogView(BuildContext context) {
-    TextEditingController _nameTec;
-    TextEditingController _desigTex;
-    TextEditingController _phoneTec;
-    TextEditingController _emailTec;
-    TextEditingController _fblinkTec;
+
+  Widget dialogViewForInput(BuildContext context) {
+    String _name;
+    String _desig;
+    String _phone;
+    String _email;
+    String _fblink;
+
+    Person newPerson =
+        Person("name", "designation", "phone", "email", "fbLink");
+
     return AlertDialog(
       actions: [
         TextButton(
             onPressed: () {
               Navigator.pop(context);
+              print("canceled");
             },
             child: Text("Cancel")),
         TextButton(
-
             onPressed: () {
+              setState(() {
+                persons.add(newPerson);
+              });
 
-
-              Person newp = Person(_nameTec.text, _desigTex.text,
-                  _phoneTec.text, _emailTec.text, _fblinkTec.text);
-              persons.add(newp);
-
-              print(persons);
-
-
-
+              Navigator.pop(context);
             },
             child: Text("Add"))
       ],
       content: Column(
         children: [
           Text("Enter Person Details"),
-          TextFormField(controller: _nameTec),
-          TextFormField(controller: _desigTex),
-          TextFormField(controller: _phoneTec),
-          TextFormField(controller: _emailTec),
-          TextFormField(initialValue: "https://fb.me/", controller: _fblinkTec),
+          TextFormField(onChanged: (value) => newPerson.name=value),
+          TextFormField(onChanged: (value) => newPerson.designation=value),
+          TextFormField(onChanged: (value) => newPerson.phone=value),
+          TextFormField(onChanged: (value) => newPerson.email=value),
+          TextFormField(onChanged: (value) => newPerson.fbLink=value ),
         ],
       ),
     );
   }
 }
-
--- lib/screens/amela.dart
+++ lib/screens/amela.dart
@@ -1,11 +1,82 @@
+import 'package:expansion_card/expansion_card.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:iscacentral/model/persondetails.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Amela extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Amela"),
+      appBar: AppBar(
+        title: Text("Amela"),
+      ),
+      body: AmelaBody(),
+    );
+  }
+}
+
+class AmelaBody extends StatefulWidget {
+  @override
+  _AmelaBodyState createState() => _AmelaBodyState();
+}
+
+class _AmelaBodyState extends State<AmelaBody> {
+  List<Person> persons = [];
+
+  @override
+  void initState() {
+    super.initState();
+    persons.add(Person("MD. Bodrul Amin", "Central Shura", "+8801725717136",
+        "bodrulaminiu@gmail.com", "https://facebook.com/bodrulamin91"));
+
+    persons.add(Person("MD. Bodrul Amin", "Central Shura", "+8801725717136",
+        "bodrulaminiu@gmail.com", "https://facebook.com/bodrulamin91"));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(itemBuilder: personCard, itemCount: persons.length);
+  }
+
+  Widget personCard(BuildContext context, int index) {
+    return Card(
+      child: ExpansionTile(
+
+        leading: FaIcon(FontAwesomeIcons.solidUser),
+        collapsedBackgroundColor: Colors.lime.shade50,
+        expandedAlignment: Alignment.bottomLeft,
+        expandedCrossAxisAlignment: CrossAxisAlignment.start,
+        childrenPadding: EdgeInsets.only(left: 20.0),
+        subtitle:   Text(persons[index].designation),
+        tilePadding: EdgeInsets.only(left: 25.0),
+        title: Text(
+          persons[index].name,
+          style: TextStyle(color: Colors.blueAccent, fontSize: 25.0),
+        ),
+        children: [
+          ListTile(
+            isThreeLine: false,
+
+               leading: Icon(Icons.phone),
+              onTap: ()=>
+                launch("tel:${persons[index].phone}"),
+              title: Text(persons[index].phone)),
+          ListTile(
+
+            leading: Icon(Icons.email),
+            onTap: () => launch("mailto:${persons[index].email}"),
+              title: Text(persons[index].email, )
+          ),
+          ListTile(
+            leading: FaIcon(FontAwesomeIcons.facebook),
+            onTap: () => launch(persons[index].fbLink),
+            title: Text(persons[index].fbLink),
+          ),
+        ],
       ),
     );
   }
+
+
 }
