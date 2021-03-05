95f9e7a5b05a8d9fa24fc86c612848fb6952f4e2
bodrulamin <bodrulaminiu@gmail.com>
2021-03-05 21:00:17 +0600

    messege 2

--- lib/main/main.dart
+++ lib/main/main.dart
@@ -9,9 +9,6 @@ import 'package:iscacentral/screens/ex.dart';
 import 'package:iscacentral/screens/loginPage.dart';
 import 'package:iscacentral/screens/shura.dart';
 
-hh
-ldg
-sdgsg
 
 void main() async {
 
